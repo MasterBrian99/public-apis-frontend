aa573010747d0d301f52c61704ea35fef159971d
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2021-09-04 19:19:36 +0530

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import Home from "./screens/Home/Home"
+import React from "react";
 
-
+import Home from "./screens/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="">
-      <Home/>
-    </div>
-  )
-}
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
